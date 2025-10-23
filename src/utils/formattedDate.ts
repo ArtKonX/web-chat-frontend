98b1a09b7acbd5251057a6b72bad0842c2f2a929
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-10-23 21:12:43 +0300

    Fix bug with date on message

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -3,7 +3,6 @@ export function formattedDate(dateData: string | number | Date | undefined) {
         const date = new Date(dateData);
 
         const formattedDate = date.toLocaleString('ru-RU', {
-            timeZone: 'UTC',
             year: 'numeric',
             month: '2-digit',
             day: '2-digit',
@@ -11,6 +10,6 @@ export function formattedDate(dateData: string | number | Date | undefined) {
             minute: '2-digit'
         });
 
-        return formattedDate
+        return formattedDate;
     }
 }
