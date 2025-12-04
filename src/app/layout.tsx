7b09b619ac836c509f249fae3e7fd6990211c277
Artemij Kondurov <kondurovartemij@yandex.ru>
2025-12-05 01:58:41 +0300

    Fix error with scale in mobile devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,12 @@ export default function RootLayout({
 
   return (
     <html lang="ru">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"
+        />
+      </head>
       <body data-testid="body" className='dark:bg-[#262626]!'>
         <Provider store={store}>
           <Suspense fallback={<Loader isFade={true} />}>
